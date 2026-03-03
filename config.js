4c7d581b1a3f9a9ddc8dd13aa797c1adc46e87bf
nifelux <globalpaysure@gmail.com>
2026-03-03 19:33:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,3 +10,9 @@ window.LOMA_CONFIG = {
     dashboard: "dashboard.html",
   }
 };
+
+// config.js
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://lpnnqxalmihxgszoifpa.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwbm5xeGFsbWloeGdzem9pZnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzM1ODEsImV4cCI6MjA4NzMwOTU4MX0.1hLW5gizjcPTKyfzx_XD9dxqegtXVQroNCclX1AaqZw",
+};
