bc550408513a1cd104d715deb4dc429a6e92189e
nifelux <globalpaysure@gmail.com>
2026-03-04 08:36:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // config.js
 window.LOMA_CONFIG = {
+  // Put your public values here (safe)
   SUPABASE_URL: "https://lpnnqxalmihxgszoifpa.supabase.co",
   SUPABASE_ANON: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwbm5xeGFsbWloeGdzem9pZnBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzM1ODEsImV4cCI6MjA4NzMwOTU4MX0.1hLW5gizjcPTKyfzx_XD9dxqegtXVQroNCclX1AaqZw",
+  PAYSTACK_PUBLIC: "pk_live_XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
